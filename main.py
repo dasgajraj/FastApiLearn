fb8695b22de598a43236db04914a574265665890
Das Gajraj Sharma <dasgajraj1@gmail.com>
2025-08-27 23:37:00 +0530

    Refactor FastAPI application: update endpoint definitions and improve response structure

--- main.py
+++ main.py
@@ -1,16 +1,45 @@
 from fastapi import FastAPI
+from fastapi import FastAPI  # Import FastAPI framework
 
-myapp = FastAPI()
+app = FastAPI()  # Create FastAPI app instance
 
-@myapp.get('/')
+@app.get('/')  # Root endpoint
 def index():
-    return {'data' : {'name': {'first': 'Das Gajraj',  'last': 'Sharma'}}}
+    """Returns a list of blogs."""
+    return {'data': 'blog-list'}
 
-@myapp.get('/about')
+@app.get('/blog')  # Endpoint to show all blogs
+def showAllBlogs(limit, published: bool):
+    """Returns published or unpublished blogs based on query params."""
+    if published:
+        return {'data': f'{limit} published blogs from DB'}
+    else:
+        return {'data': f'{limit} unplblished blogs from DB'}
+
+@app.get('/about')  # About endpoint
 def about():
-    return {'data' : {'age': 21, 'location': 'India'}}
+    """Returns information about the author."""
+    return {'data': {'age': 21, 'location': 'India'}}
 
-@myapp.get('/social-media')
+@app.get('/social-media')  # Social media endpoint
 def social_media():
-    return {'data' : {'twitter': '@dasgajraj', 'linkedin': 'in/dasgajraj'}}
+    """Returns social media handles."""
+    return {'data': {'twitter': '@dasgajraj', 'linkedin': 'in/dasgajraj'}}
+
+@app.get('/blog/unpublished')  # Endpoint for unpublished blogs
+def unpublished():
+    """Returns all unpublished blog data."""
+    return {'data': "All Unpublished Data"}
+
+@app.get('/blog/{id}')  # Endpoint for blog by id
+def show(id: int):
+    """Returns blog data for a given id."""
+    return {'data': id}
+
+@app.get('/blog/{id}/comments')  # Endpoint for comments of a blog
+def comments(id):
+    """Returns comments for a given blog id."""
+    return {'data': {'1', '2'}}
+
+# completed 50 mins..
 
